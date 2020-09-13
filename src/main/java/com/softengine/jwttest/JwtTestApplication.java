6a2de26c2b6aadbcd55185698ce0554488b5ee35
MEHMET ONAR <mhmtonar1@gmail.com>
2020-09-13 20:24:04 +0300

    postgresql

--- src/main/java/com/softengine/jwttest/JwtTestApplication.java
+++ src/main/java/com/softengine/jwttest/JwtTestApplication.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
@@ -19,6 +20,7 @@ import java.util.stream.Stream;
 
 @Slf4j
 @SpringBootApplication
+@EnableJpaRepositories
 public class JwtTestApplication {
 
     @Autowired
